7ad1e2e00fb7b11dc7790c895080bebb4885acef
Ren-hongchen <1280656287@qq.com>
2022-06-14 20:02:22 +0800

    aside

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <el-button>登录</el-button>
+    <el-container style="height: 100%">
+      <el-aside width="auto">
+        <common-aside></common-aside>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import CommonAside from '../components/CommonAside.vue'
 export default {
   name: 'Index',
+  components: {
+    CommonAside
+  },
   data () {
     return {}
   }
 }
 </script>
+
+<style lang="less">
+.el-header {
+  background-color: #333;
+}
+.el-main {
+  padding-top: 0;
+}
+</style>
